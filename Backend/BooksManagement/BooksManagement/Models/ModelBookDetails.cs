59128cbef8756c780161b1d46ff59b47eaa979bf
Janindumaleesha <janindu.maleesha@app360.lk>
2025-05-23 21:12:37 +0530

    Created basic project.

--- Backend/BooksManagement/BooksManagement/Models/ModelBookDetails.cs
+++ Backend/BooksManagement/BooksManagement/Models/ModelBookDetails.cs
@@ -1,6 +1,23 @@
-﻿namespace BooksManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksManagement.Models
 {
     public class ModelBookDetails
     {
+        public int? Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100)]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Author is required")]
+        [StringLength(100)]
+        public string? Author { get; set; }
+
+        [Required(ErrorMessage = "ISBN is required")]
+        [StringLength(50)]
+        public string? ISBN { get; set; }
+
+        public DateTime? PublicationDate { get; set; }
     }
 }
